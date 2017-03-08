07a6caff7904b5aac43ac0c3692f68eccc46e665
Paul Karabilo <benizebe@gmail.com>
2017-03-08 19:52:49 +0100

    start

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <time.h>
+
+void header() {
+    printw("Welcome to ROBCO Industries (TM) Termlink\n");
+    printw("Password required\n");
+    printw("Attempts remaining: | | | |\n");
+}
+
+void loop() {
+    int ch = getch();
+    while (ch != 27) {
+        if (ch == KEY_RIGHT) {
+            printw("right");
+        } else if (ch == KEY_LEFT) {
+            printw("left");
+        } else if (ch == KEY_DOWN) {
+            printw("down");
+        } else if (ch == KEY_UP) {
+            printw("up");
+        } else if (ch == KEY_ENTER) {
+            printw("enter");
+        } else {
+            printw("%c", ch);
+        }
+        printw("\n");
+        refresh();
+        ch = getch();
+    }
+}
+
+int main(int argc, char** argv) {
+    initscr();
+    raw();
+    keypad(stdscr, TRUE);
+    noecho();
+    header();
+    loop();
+    
+    endwin();
+    return (EXIT_SUCCESS);
+}
+
---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	gcc main.c -o build/f4h -lncurses
